eb248575480c246a01e30eb33f2ee5c9e62cbfbc
Mattmon <MattmonGaming@gmail.com>
2022-07-05 00:35:11 -0600

    Added install script for 30 series
    added a script to install pixray and update the packages so it will work with NVIDIA 30 series cards

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#script to install pixray and update the packages for an NVIDIA 30 series card(s)
+git clone --recursive https://github.com/pixray/pixray 
+
+cd pixray/ 
+
+pip install -r requirements.txt
+pip install basicsr 
+
+pip --no-cache-dir install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+
+git clone https://github.com/pixray/diffvg
+cd diffvg  
+git submodule update --init --recursive
+python setup.py install
